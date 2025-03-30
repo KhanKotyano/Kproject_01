f4e8dda603f2b46ed6d2b771fcfdd8e28f4a43dd
Mondanachr <levedikangel@gmail.com>
2025-03-30 15:40:04 +0600

    test

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 if exist %~dp0\\build\\*.exe del %~dp0\\build\\*.exe
-gcc da.c -o %~dp0build\\main.exe -O2 -Wall -std=c11 -Wno-missing-braces -I ./include/ -L ./lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
+clang %~dp0*.c -o %~dp0build\\main.exe -O2 -Wall -std=c11 -Wno-missing-braces -I ./include/ -L ./lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
 cmd /c if exist %~dp0build\\main.exe %~dp0build\\main.exe
